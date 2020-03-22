1a23ad1957ca33811a29fb51445a9894f95f77a5
Leonid Petrov <lp.msarchitect@gmail.com>
2020-03-22 21:13:38 +0300

    feat: Add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    const bracketsArray = str.split("");
+    for (let i = 0; i < bracketsArray.length; i++) {
+        for (let j = 0; j < bracketsConfig.length; j++) {
+            if (
+                bracketsArray[i] === bracketsConfig[j][0] &&
+                bracketsArray[i + 1] === bracketsConfig[j][1]
+            ) {
+                const currentPosition = i;
+                bracketsArray.splice(i, 2);
+                i = -1;
+            }
+        }
+        if (bracketsArray.length == 0) {
+            return true;
+        }
+    }
+    return false;
+};
